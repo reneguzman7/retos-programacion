20c1c31437cbdb27cebd756b0aa65903e7715a41
reneguzman7 <121918879+reneguzman7@users.noreply.github.com>
2023-10-20 23:03:12 -0500

    Resolución reto #0 #1 #2 - 2022

---
+++ ejercicios-2022/challenge0.js
@@ -0,0 +1,23 @@
+/*
+ * Reto #0
+ * EL FAMOSO "FIZZ BUZZ"
+ * Fecha publicación enunciado: 27/12/21
+ * Fecha publicación resolución: 03/01/22
+ * Dificultad: FÁCIL
+ * Enunciado: Escribe un programa que muestre por consola (con un print) los números de 1 a 100 (ambos incluidos y con un salto de línea entre cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ *
+ */
+
+for (let num = 1; num <= 100; num++) {
+    if (num % 3 === 0 && num % 5 === 0)
+        console.log('fizzbuzz')
+    else if (num % 3 === 0)
+        console.log('fizz')
+    else if (num % 5 === 0)
+        console.log('buzz')
+    else
+        console.log(num)
+}
---
+++ ejercicios-2022/challenge2.js
@@ -0,0 +1,26 @@
+/*
+ * Reto #2
+ * LA SUCESIÓN DE FIBONACCI
+ * Fecha publicación enunciado: 10/01/22
+ * Fecha publicación resolución: 17/01/22
+ * Dificultad: DIFÍCIL
+ *
+ * Enunciado: Escribe un programa que imprima los 50 primeros números de la sucesión de Fibonacci empezando en 0.
+ * La serie Fibonacci se compone por una sucesión de números en la que el siguiente siempre es la suma de los dos anteriores.
+ * 0, 1, 1, 2, 3, 5, 8, 13...
+ */
+
+function fibonacci(numberOfRepetitions) {
+    let result = []
+    num1 = 0
+    num2 = 1
+    for (let i = 0; i < numberOfRepetitions; i++) {
+        result.push(num1)
+        let temp = num1
+        num1 = num2
+        num2 += temp
+    }
+    return result
+}
+
+console.log(fibonacci(50))
